a6fe834a811a68a73b4d62f3e6e52ed1006f5082
shareenshaik143 <112694302+shareenshaik143@users.noreply.github.com>
2022-09-15 11:58:28 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "LocationFetcher"
+include(":locationfetcher")
+include(":locationfetcher-compose")
+include(":app")
